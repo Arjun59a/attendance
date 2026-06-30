acf914dbadf5ea816dd948000d7ced5fd549e45d
Arjun_59a <at3903334@gmail.com>
2026-06-30 22:40:01 +0530

    Fix configuration matching and optimize offline persistence buffers

--- lighter/social_media/content/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("home/<str:username>",views.homepage , name="home"),
-    path("get_content/<str:username>",views.getting_content,name="Getpost"),
-    path("likedislike/<int:id>/<str:username>",views.like_dislike,name="like")
-]
